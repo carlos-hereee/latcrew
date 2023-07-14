374783ef5757d865938ac05815b64deb341d44e5
ch04937 <97hernandez.c@gmail.com>
2023-07-13 19:34:37 -0500

    landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,13 +2,12 @@ import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { HeroCard, FeatureCard } from "nexious-library/@nxs-organism";
 import { loadAsset } from "../assets/getUrl";
-
+import { Socials } from "nexious-library/@nxs-molecules";
 const Landing = () => {
-  const { app } = useContext(AppContext);
-
+  const { app, socials } = useContext(AppContext);
   return (
-    <main>
-      <div>
+    <main className="flex-d-column">
+      <div className="flex-d-column">
         <HeroCard
           heading={app.heading}
           hero={{ ...app.hero, url: loadAsset(`${app.hero.url}`) }}
@@ -17,7 +16,11 @@ const Landing = () => {
         />
         <p className="p-stretch">{app.description}</p>
       </div>
-      <div className="flex-j-between">
+      <Socials
+        data={socials}
+        heading="Dont miss a thing! Follow us on our socials"
+      />
+      <div className="feature-card-container">
         {app.features.map((af) => (
           <FeatureCard
             feature={af}
