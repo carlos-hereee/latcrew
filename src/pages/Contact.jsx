4cb6e11710d41f151455e1ea5b86fc6b78315edf
ch04937 <97hernandez.c@gmail.com>
2023-07-15 18:36:04 -0500

    add contact info

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,10 @@
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { HeroCardAlt } from "nexious-library/@nxs-organism";
+import { HeroCardAlt, CardSection } from "nexious-library/@nxs-organism";
 import { loadAsset } from "../assets/getUrl";
 const Contact = () => {
   const { contact } = useContext(AppContext);
-  console.log("contact", contact);
+  console.log("contact", contact.contact);
   return (
     <div className="container">
       <HeroCardAlt
@@ -12,6 +12,19 @@ const Contact = () => {
         hero={{ ...contact.hero, url: loadAsset(contact.hero.url) }}
         body={contact.body}
       />
+      <div className="contact-cards">
+        {contact.contact.map((c) => (
+          <CardSection
+            key={c.uid}
+            header={{
+              title: c.name,
+              hasHero: true,
+              hero: { ...c.hero, url: loadAsset(c.hero.url) },
+            }}
+            body={c.body}
+          />
+        ))}
+      </div>
     </div>
   );
 };
