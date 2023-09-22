7c3d135213ff88ce1b66020ba2eb3c3f0cadb605
ch04937 <97hernandez.c@gmail.com>
2023-09-21 21:33:27 -0500

    add section form data

--- src/components/app/BuildApp.jsx
+++ src/components/app/BuildApp.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../utils/context/app/AppContext";
 import { PaginateForm } from "nexious-library/@nxs-template";
 import { AuthContext } from "../../utils/context/auth/AuthContext";
@@ -8,44 +8,65 @@ const BuildApp = ({ heading, cancelBtn, onClick }) => {
   const { landingPageValues, landingPageLabels, sectionValues } = useContext(AppContext);
   const { landingPageTypes } = useContext(AppContext);
   const { buildApp } = useContext(AuthContext);
+  const [formPage, setFormPage] = useState(0);
+  const [paginate, setPaginate] = useState([
+    {
+      formName: "appName",
+      heading: "Initialize your app",
+      initialValues: { appName: "Sharkle and Shine" },
+      submitLabel: "Save and continue",
+      schema: { required: ["appName"] },
+    },
+    {
+      formName: "landingPage",
+      heading: "Build landing page",
+      initialValues: landingPageValues,
+      submitLabel: "Save and continue",
+      labels: landingPageLabels,
+      types: landingPageTypes,
+    },
+  ]);
 
-  const initFormValues = (formName, data) => {};
-
+  const initSubSection = (hasCTA) => {
+    setPaginate((prev) => [
+      ...prev,
+      {
+        formName: "section",
+        heading: "Additional features",
+        submitLabel: hasCTA ? "Save and continue" : "Publish app",
+        initialValues: sectionValues,
+      },
+    ]);
+  };
+  const initCta = (hasSubSection) => {
+    setPaginate((prev) => [
+      ...prev,
+      {
+        formName: "cta",
+        heading: "Call To Action",
+        submitLabel: hasSubSection ? "Save and continue" : "Publish app",
+        initialValues: sectionValues,
+      },
+    ]);
+  };
   const handleFormSubmit = (event) => {
     // search for sub forms
     const hasCTA = event.landingPage.cta;
     const hasSubSection = event.landingPage.sections;
     if (hasCTA || hasSubSection) {
-      hasSubSection && console.log("hasCTA", hasCTA, "has sub section", hasSubSection);
-      hasCTA && console.log("hasCTA", hasCTA, "has sub section", hasSubSection);
-      // console.log("handle paginated form submit event", event);
-    } else {
-      buildApp(event);
-    }
+      if (hasCTA) initCta(hasSubSection);
+      if (hasSubSection) initSubSection(hasCTA);
+      setFormPage((prev) => prev + 1);
+    } else buildApp(event);
   };
   return (
     <div className="container">
       {heading && <h2 className="heading">{heading}</h2>}
       <PaginateForm
         // order={["landingPage", "appName"]}
-        paginate={[
-          {
-            formName: "appName",
-            heading: "Initialize your app",
-            // initialValues: { appName: "" },
-            initialValues: { appName: "Sharkle and Shine" },
-            submitLabel: "Save and continue",
-            schema: { required: ["appName"] },
-          },
-          {
-            formName: "landingPage",
-            heading: "Build landing page",
-            initialValues: landingPageValues,
-            submitLabel: "Save and continue",
-            labels: landingPageLabels,
-            types: landingPageTypes,
-          },
-        ]}
+        page={formPage}
+        setNewPage={setFormPage}
+        paginate={paginate}
         onFormSubmit={handleFormSubmit}
       />
       {cancelBtn && (
