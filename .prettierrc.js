0feaa65bd1ddccbf67ce0936037159ccac71e0bf
ch04937 <97hernandez.c@gmail.com>
2023-09-26 18:12:08 -0500

    eslint with prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  // more space make it readable
+  printWidth: 100,
+};
+
+export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import AppRouter from "./AppRouter";
 import "./index.css";
 import { ServicesState } from "./utils/context/services/ServicesContext";
