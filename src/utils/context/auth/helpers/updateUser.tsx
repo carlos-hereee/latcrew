e595c6752d74f7f816c23a3e4a9187fafe107b9c
ch04937 <97hernandez.c@gmail.com>
2023-10-17 16:06:10 -0500

    auth actions in action

--- src/utils/context/auth/helpers/updateUser.tsx
+++ src/utils/context/auth/helpers/updateUser.tsx
@@ -15,5 +15,4 @@ export const updateUser = (props: UpdateUserReducerProps) => {
   dispatch({ type: AUTH_ACTIONS.SET_USER_DATA, payload: userData });
   user.ownedApps && dispatch({ type: AUTH_ACTIONS.SET_OWNED_APPS, payload: user.ownedApps });
   user.permissions && dispatch({ type: AUTH_ACTIONS.SET_PERMSSIONS, payload: user.permissions });
-  // user.permission.length > 0 && dispatch({ type: AUTH_ACTIONS.SET_IS_ADMIN, payload: true });
 };
